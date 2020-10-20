adf5dc4906bab2a43cda6b5b992fef32751e5f6a
Mateusz Kielbasa <P17181660@my365.dmu.ac.uk>
2020-10-20 19:50:54 +0200

    added project section

--- mainj.js
+++ mainj.js
@@ -31,4 +31,50 @@ navLinks.forEach(link => {
             }
         })
     })
-})
+});
+
+/*
+
+  P1 100%, P2, P3 opacity 0%
+  P1 80% P2 100, P3 0
+  P1 0, P2 80, P3 100
+  P1 P2 0 P3 100
+  P1 P2 P3 0
+
+
+ */
+
+
+/* THis is so image stays still for project 1 */
+
+window.addEventListener("scroll",()=>{
+//  console.log(document.documentElement.scrollTop);
+
+  const  image =document.getElementById("project-Image1");
+  const test = document.getElementById("Project-text");
+  const  p1 =document.getElementById("project1_p1");
+  const  p2 =document.getElementById("project1_p2");
+  const  p3 =document.getElementById("project1_p3");
+  p1.style.opacity ="100%";
+  p2.style.opacity ="0%";
+  p3.style.opacity ="0%";
+
+
+ const minScroll=4250;
+ const maxScroll=5890; //limit
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if(scrollTop> minScroll && scrollTop<maxScroll){
+    p1.style.opacity= 1-scrollTop/maxScroll;
+    test.style.position="fixed";
+  //  p1.style.left="60%";
+    image.style.position ="fixed";
+    //image.style.marginLeft: "40px";
+  //  image.style.marginTop: "120px";
+    p1.style.opacity
+  //  console.log("FIXED");
+  }else {
+    image.style.position ="relative";
+  p1.style.position="relative";
+    //console.log("RELATIVE");
+  }
+});
